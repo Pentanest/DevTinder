a15099a07bfdffa7927a4142e1e766ada374c7fd
Shubhankar <shubhankar.debnath@spcnc.com>
2024-10-06 19:22:02 +0530

    Created more APIs

--- src/app.js
+++ src/app.js
@@ -2,19 +2,79 @@ const express = require("express");
 const connectDB = require("./config/database");
 const PORT = 7777;
 const User = require("./models/user");
+const user = require("./models/user");
 
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const userObj = { firstName: "Asheesh", lastName: "Sahu", emailId: "asheesh@sahu.com", password: "asheesh@123" };
+app.use(express.json());
 
-  const user = new User(userObj); // creating a new instance of the user model
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body); // creating a new instance of the user model
 
   try {
     await user.save();
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error adding user: ", err?.message);
+  }
+});
+
+// app.get("/user", async (req, res) => {
+//   const { emailId } = req.body;
+//   try {
+//     const user = await User.findOne({ emailId });
+//     res.send(user);
+//   } catch (err) {
+//     res.status(400).send("Error fetching user: ", err.message);
+//   }
+// });
+
+// Get user by emailId
+app.get("/user", async (req, res) => {
+  const { emailId } = req.body;
+  try {
+    const user = await User.findOne({ emailId });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Error fetching user: ", err.message);
+  }
+});
+
+// Get all the users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length) {
+      res.send(users);
+    } else {
+      res.status(404).send("No users found");
+    }
+  } catch (err) {
+    res.status(400).send("Error fetching users: ", err.message);
+  }
+});
+
+// Delete user by _id
+app.delete("/user", async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Error deleting user: ", err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const { userId } = req.body;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, { returnDocument: "after" });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
